aa16dc458e606674502f5cc6853fe70f1b3b7e53
mh-fs <matheus.henrique1021@gmail.com>
2020-01-28 20:59:58 -0300

    Fifth online exercise

---
+++ online/ex5.js
@@ -0,0 +1,7 @@
+// solicitamos que você declare 4 variáveis ​​usando o let com os nomes: verdadeiro, nada, texto e número. Para cada um deles, 
+// você deve atribuir o tipo de dados que sugere seu nome, conforme apropriado.
+
+let verdadeiro = true;
+let nada = null;
+let texto = "texto";
+let numero = 0; 
