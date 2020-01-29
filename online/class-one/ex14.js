1223364d9db8f0a77b0d41850cb69a4494f48dbc
mh-fs <matheus.henrique1021@gmail.com>
2020-01-29 01:51:28 -0300

    Fourteenth online exercise

---
+++ online/class-one/ex14.js
@@ -0,0 +1,11 @@
+// Seu trabalho agora é construir uma condicional if / else para que, se a variável contém "javascript" imprima no console o texto 
+// "Estou aprendendo", se não imprima "Vou aprender mais pra frente".
+
+let linguagem = "javascript";
+function contemString(){
+    if(linguagem === "javascript"){
+        console.log("Estou aprendendo");
+    }else{
+        console.log("Vou aprender mais pra frente");
+    }
+}
