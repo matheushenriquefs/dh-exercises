f4241ba705d47365c23a9670df548d30b4cc4447
mh-fs <matheus.henrique1021@gmail.com>
2020-01-29 11:24:29 -0300

    Seventeenth online exercise

---
+++ online/class-one/ex17.js
@@ -0,0 +1,23 @@
+// O objetivo deste exercício é imprimir um texto na tela, que será condicionado da seguinte maneira.Usando "switch" você deve avaliar 
+// se a variável "dia" é segunda, quarta ou sexta-feira e, nesse caso, você deve imprimir o texto "você tem aulas!". 
+// Para qualquer outro caso, você deve imprimir "você não tem aulas".
+
+let dia = "sabado";
+
+function fimDeSemana(dia){
+	switch(dia){
+        case "segunda":
+            console.log("você tem aulas!");
+            break;
+        case "quarta":
+            console.log("você tem aulas!");
+            break;
+        case "sexta":
+            console.log("você tem aulas!");
+            break;
+        default:
+            console.log("você não tem aulas");
+    }
+}
+
+fimDeSemana(dia);
