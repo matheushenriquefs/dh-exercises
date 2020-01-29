4d3cd8c163af7c98911fd28a8ac2b9fc86d60cb7
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-01-29 16:38:09 -0300

    Add more content to second exercise

--- presential/class-one/ex2.js
+++ presential/class-one/ex2.js
@@ -1,26 +1,55 @@
 // sistema petshop
 
 const NOME_PETSHOP = "Petshop DH";
-// console.log(NOME_PETSHOP);
 let nome = "Malu";
 let idade = 3;
 let raca = "Shih-Tzu";
 let vacinado = true;
 let servicos = ["banho", "tosa"];
-// console.log(nome, idade, raca, vacinado, servicos);
 
-let cachorro = {
+let caes = [{
     nome: "Malu",
     idade: 3,
     raca: "Shih-Tzu",
     vacinado: true,
     servicos: ["banho", "tosa"]
-}
+},
+{
+    nome: "Batman",
+    idade: 5,
+    raca: "Doberman",
+    vacinado: true,
+    servicos: ["banho", "tosa"]
+},
+{
+    nome: "SuperMan",
+    idade: 4,
+    raca: "Pastor Alemão",
+    vacinado: false,
+    servicos: ["banho", "tosa"]
+}]
 
-// console.log(cachorro);
-
-function eVacinado(){
-    let result = cachorro.vacinado ? "É vacinado": "Não é vacinado";
+function eVacinado(cao){
+    let result = cao.vacinado ? true: false;
     return result;
 }
-console.log(eVacinado());
+
+function adicionarCao(arrayObj, nome, idade, raca, vacinado, servicos){
+    arrayObj.push({
+        nome: nome,
+        idade: idade,
+        raca: raca,
+        vacinado: vacinado,
+        servicos: servicos
+    });
+}
+
+adicionarCao(caes, "Lanterna Verde", 8, "Pitbull", true, ["banho", "tosa"]);
+
+function listarCaesVacinados(arrayObj){
+    for(let i = 0; i < arrayObj.length; i++){
+        if(arrayObj[i].vacinado){
+            console.log(arrayObj[i].nome + " é vacinado(a)");
+        }
+    }
+}
