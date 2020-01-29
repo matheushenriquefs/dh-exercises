0fa85722001f421950a690c927f5e2ad039ac8d0
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-01-29 15:35:01 -0300

    First presential exercise

---
+++ presential/class-one/ex1.js
@@ -0,0 +1,15 @@
+// tipos de variaveis
+
+var minhaVariavel;
+console.log(minhaVariavel);
+minhaVariavel = "Hello World";
+console.log(minhaVariavel);
+
+let minhaVariavel2 = true;
+minhaVariavel2 = false;
+console.log(minhaVariavel2);
+
+const ESCOLA_DIGITAL = "Digital House";
+console.log(ESCOLA_DIGITAL);
+// erro
+// ESCOLA_DIGITAL = "Outra Escola";
