beb61ee82faf38be2cc7119a72fb3b1dfc12781d
mh-fs <matheus.henrique1021@gmail.com>
2020-01-29 11:57:44 -0300

    Eighteenth online exercise

---
+++ online/class-one/ex18.js
@@ -0,0 +1,7 @@
+// A função se chama papagaio e deve receber como parâmetro um texto e repetir 5 vezes no console.
+
+function papagaio(string){
+    for(let i = 0; i < 5; i++){
+        console.log(string);
+    }
+}
