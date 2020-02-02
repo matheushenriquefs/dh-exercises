656432ccab4694aed595492f671b69ff3e9d89d6
mh-fs <matheus.henrique1021@gmail.com>
2020-02-02 17:11:14 -0300

    Add exercise statement

--- online/class-two/ex22.js
+++ online/class-two/ex22.js
@@ -1,3 +1,6 @@
+// Criar uma função chamada dominio que receberá uma String  "br.digitalhouse.com" e sua função será devolver 
+// "http://br.digitalhouse.com".
+
 function dominio(string){
     return "http://".concat(string);
 }
--- online/class-two/ex23.js
+++ online/class-two/ex23.js
@@ -1,2 +1,5 @@
+// declarar uma variável chamada "texto" atribuindo como valor um texto,  que você preferir. Em seguida, usando a propriedade length, 
+// você precisará imprimir no console o total de caracteres que a frase contém.
+
 let texto = "texto";
 console.log(texto.length);
