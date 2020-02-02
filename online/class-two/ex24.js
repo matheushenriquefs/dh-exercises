b2b2cc65f1d3cf6a5f557d31e6604ece73b68228
mh-fs <matheus.henrique1021@gmail.com>
2020-02-02 17:18:42 -0300

    Twenty-fourth online exercise

---
+++ online/class-two/ex24.js
@@ -0,0 +1,6 @@
+// criar uma função chamada "substituicaoRapida" que você recebe um texto, uma palavra para procurar e a palavra que substituirá a 
+// que você está procurando. A função deve retornar o texto com a palavra substituída.
+
+function substituicaoRapida(texto, procurar, substituir){
+    return texto.replace(procurar, substituir);
+}
