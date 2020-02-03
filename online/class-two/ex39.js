8beee98b504d51fdf97586510f8aa91bf3ca70c6
mh-fs <matheus.henrique1021@gmail.com>
2020-02-03 20:42:27 -0300

    Thirty-ninth online exercise

---
+++ online/class-two/ex39.js
@@ -0,0 +1,8 @@
+// Temos uma frase declarada e queremos experimentar um pouco.
+// Imprima no console cada letra para que a frase seja escrita na vertical.
+
+let frase = "Essa semana vou no colearning";
+
+for(let value of frase){
+    console.log(value);
+}
