a16fd4779cebb2ce8b3620a7cd08a6f80dd3a078
mh-fs <matheus.henrique1021@gmail.com>
2020-02-03 18:04:15 -0300

    Thirty-first online exercise

---
+++ online/class-two/ex31.js
@@ -0,0 +1,9 @@
+// Para este exercício contamos com o array alunos. Queremos saber onde estão guardados os nomes de "João" e "Francisco". Utilize 
+// um dos métodos de array.
+// Declare as variáveis  indiceJoao, indiceFrancisco e armazene a posição correspondente de cada um.
+
+let alunos = ["João", "Pedro", "Jorge", "Francisco"];
+
+let indiceJoao = alunos.indexOf("João");
+
+let indiceFrancisco = alunos.indexOf("Francisco");
