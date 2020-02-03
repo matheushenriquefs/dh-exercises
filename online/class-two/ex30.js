99eebe03e5ee66840135f229b486d6c8fac97786
mh-fs <matheus.henrique1021@gmail.com>
2020-02-03 17:29:53 -0300

    Thirtieth online exercise

---
+++ online/class-two/ex30.js
@@ -0,0 +1,6 @@
+// Para este exercício, oferecemos várias marcas de carros e queremos que você exiba a marca "Ford" por console. Você deve acessar o 
+// elemento correspondente da array para resolvê-lo.
+
+let marcasDeAutos = ["Mustang", "Ferrari", "Ford", "Chevrolet"];
+
+console.log(marcasDeAutos[2]);
