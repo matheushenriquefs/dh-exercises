aa685a6f4209757b90f24e0e4d6e1b7555c1e3e8
mh-fs <matheus.henrique1021@gmail.com>
2020-02-03 18:43:52 -0300

    Thirty-fourth online exercise

---
+++ online/class-two/ex34.js
@@ -0,0 +1,34 @@
+// Seu trabalho será acrescentar um aluno de cada vez ao array de estudantes. Utilize um dos métodos de array.
+
+// Os valores de cada aluno são:
+
+// nome: João
+
+// media: 5
+
+// curso: iOS
+
+// ------------------
+
+// nome: Miguel
+
+// media: 2
+
+// curso: Android
+
+let estudantes = [
+    {
+      nome: 'Álvaro',
+      media : 9,
+      curso : 'Android',
+    },
+    {
+      nome: 'Daniel',
+      media : 6,
+      curso : 'Full Stack',
+    }
+]
+
+estudantes.push({nome: "João", media: 5, curso: "iOS"});
+
+estudantes.push({nome: "Miguel", media: 2, curso: "Android"});
