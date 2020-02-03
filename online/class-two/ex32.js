63bdfa9020ff17bf91a48feeb3c258925898b061
mh-fs <matheus.henrique1021@gmail.com>
2020-02-03 18:23:04 -0300

    Thirty-second online exercise

---
+++ online/class-two/ex32.js
@@ -0,0 +1,5 @@
+// Temos um array com palavras soltas que, juntas, formam uma grande frase. Queremos que, utilizando um método de array em arrayFrase ,
+// o converta em uma frase inteira. Queremos também que armazene essa oração em uma variável do tipo String chamada fraseNova.
+
+let arrayFrase = ["Não fracassei,", "simplesmente", "encontrei", "dez", "mil", "soluções", "equivocadas"];
+let fraseNova = arrayFrase.join(" ");
