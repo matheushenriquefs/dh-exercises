6b9757d53e09b5e5d6009faebad6a5e2f36bc74c
mh-fs <matheus.henrique1021@gmail.com>
2020-02-05 01:09:57 -0300

    Fortieth online exercise

---
+++ online/class-three/ex40.js
@@ -0,0 +1,12 @@
+// Você vai criar agora a sua primeira data com  Date... a que você quiser!
+
+// Para começar, você deve criar uma variável data e armazenar nela uma instancia de Date.
+
+// Depois, você terá que configurar a data utilizando os métodos setYear, setMonth e setDate, passando a eles os argumentos correspondentes
+// a cada um.
+
+let data = new Date();
+data.setFullYear(2001);
+data.setMonth(8);
+data.setDate(27);
+console.log(data);
