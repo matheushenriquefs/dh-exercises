dddb8c81cf9dec9e950eda82d97ef8880355bf56
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-07 15:16:39 -0300

    Update functions.js

--- presential/class-three/functions.js
+++ presential/class-three/functions.js
@@ -1,32 +1,34 @@
 let pets = require('./pets.json');
-console.log(pets);
 
 // transformando strings de data em objetos date
 
-// pets = pets.nascimento.map(el => {
-
-// });
+pets = pets.map(pet => {
+    pet.nascimento = new Date(pet.nascimento);
+    return pet;
+});
 
 const servDarBanho = pets.map(pet => {
     console.log("Dando banho em: " + pet.nome);
     pet.servicos.push("banho");
     return pet;
 });
-console.log(servDarBanho);
 
 const servTosar = pets.map(pet => {
     console.log("Tosando: " + pet.nome);
     pet.servicos.push("tosar");
     return pet;
 });
-console.log(servTosar);
+
+// aplicando a funcao serv vacinar a todos os pets
 
 const servVacinar = pets.map(pet =>{
     console.log("Vacinando: " + pet.nome);
-    if(pet.vacinado !== true){
-        pet.vacinado = true;
-        pet.servicos.push("vacina");
-    }
+    // if(pet.vacinado !== true){
+    //     pet.vacinado = true;
+    //     pet.servicos.push("vacina");
+    // }
+    pet.vacinado = true;
     return pet;
 });
+
 console.log(servVacinar);
