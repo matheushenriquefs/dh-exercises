c67bf67cee607156fa36a614e370bda572f7983d
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-12 17:32:40 -0300

    Create destructuring.js

---
+++ presential/class-four/destructuring.js
@@ -0,0 +1,10 @@
+let produto = {
+    empresa: "Nestlé",
+    cnpj: "12.123.123/0001-25",
+    nome: "KitKat",
+    peso: 41.5,
+    validade: new Date("2021-10-15")
+}
+
+let {nome, peso} = produto;
+console.log(nome, peso);
