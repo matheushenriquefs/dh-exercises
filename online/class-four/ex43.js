00edc93c6af33458339964168e1600530961f16c
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:05:50 -0300

    Update ex43.js

--- online/class-four/ex43.js
+++ online/class-four/ex43.js
@@ -1,3 +1,6 @@
+// Temos dados do cachorro Scooby armazenados em diversas variáveis. Seu trabalho será criar a variável cachorro e armazenar nela
+// todos os dados e depois, exporta-los.
+
 let cachorro = {
     nome: 'Scooby',
     idade: 7,
