562f1771baf8a6a856f6c4b51124952031ae7b9f
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:10:58 -0300

    Update ex51.js

--- online/class-four/ex51.js
+++ online/class-four/ex51.js
@@ -1,3 +1,6 @@
+// Seu trabalho aqui será instanciar um servidor na porta 8000. Por sua vez, queremos que a frase "Levantando um servidor com 
+// Express" seja exibida no console quando o servidor estiver rodando, para verificar se foi configurado corretamente.
+
 const express = require('express');
 const app = express();
 
