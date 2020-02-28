83ff15cb80a6b0fe782a7def73e5f3807ed8e86e
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:11:30 -0300

    Update ex52.js

--- online/class-four/ex52.js
+++ online/class-four/ex52.js
@@ -1,3 +1,5 @@
+// Seu trabalho será, utilizando o método GET, criar uma rota para "/home" que devolva a frase "Olá, estamos na página home". 
+
 const express = require('express');
 const app = express();
 
