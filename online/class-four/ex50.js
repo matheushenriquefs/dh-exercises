0fea7f6da08227a06115a2ae5a41af4bdeb8e4fd
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:10:20 -0300

    Update ex50.js

--- online/class-four/ex50.js
+++ online/class-four/ex50.js
@@ -1,2 +1,5 @@
+// Pedimos que, como primeiro passo, crie a variável express e armazene nela o módulo express. Depois, crie a variável app e 
+// armazene nela a execução da função de alto nível express().
+
 const express = require("express");
 const app = express();
