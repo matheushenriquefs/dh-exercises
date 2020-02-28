1dc107d2eeed9d50967a40e2be881d2219bd7f2a
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:07:17 -0300

    Update ex45.js

--- online/class-four/ex45.js
+++ online/class-four/ex45.js
@@ -1,1 +1,4 @@
+// Vamos instanciar um servidor, pois a primeira coisa que precisamos fazer é um require para incluir um outro módulo. 
+// Pedimos, como primeiro passo, para criar a variável "http" e armazenar o módulo http nele.
+
 const http = require("http");
