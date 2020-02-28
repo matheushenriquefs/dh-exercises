1a1bef5d555b809fc14d44cc17467d6c4d96fd09
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:07:54 -0300

    Update ex46.js

--- online/class-four/ex46.js
+++ online/class-four/ex46.js
@@ -1,3 +1,6 @@
+// Queremos que você execute o método necessário para criar o nosso servidor.
+// Este método recebe um callback. Faça que o mesmo imprima no console a frase "Instanciando um servidor"
+
 const http = require('http');
 
 http.createServer((req, res) =>  {
