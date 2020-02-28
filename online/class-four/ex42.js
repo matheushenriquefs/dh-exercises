373ffb97f6d15725bdb99f46b28584ce53b0253b
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:05:12 -0300

    Update ex42.js

--- online/class-four/ex42.js
+++ online/class-four/ex42.js
@@ -1,3 +1,7 @@
+// Seu objetivo será criar 3 variáveis: "moduloNativo", "moduloInstalado" e "moduloProprio", onde cada uma armazenará um módulo 
+// distinto. A primeira deve requisitar o módulo http, a segunda o módulo axios e a terceira um módulo local exportado do arquivo 
+// minhaFuncao.js. Este arquivo se encontra na mesma pasta que o arquivo index.js.
+
 const moduloNativo = require("http");
 const moduloInstalado = require("axios");
 const moduloProprio = require("./minhaFuncao");
