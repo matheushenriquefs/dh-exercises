0da82da95b07a4e8b19f0995ec3a27d792105fcf
Matheus Henrique <44712248+matheushenriquefs@users.noreply.github.com>
2020-02-28 01:09:45 -0300

    Update ex49.js

--- online/class-four/ex49.js
+++ online/class-four/ex49.js
@@ -1,3 +1,6 @@
+// Seu trabalho será instanciar um servidor na porta 8000. Queremos que, quando o sevidor inicie, você imprima no console a 
+// frase: "Levantando o servidor com Express" . Isso fará com que você tenha certeza de que tudo correu bem.
+
 const express = require('express');
 const app = express();
 
