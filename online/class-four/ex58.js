883e09dd164b7ccc8b9224b191b131897d277e39
mh-fs <matheus.henrique1021@gmail.com>
2020-03-07 17:34:38 -0300

    Fifty-eighth online exercise

---
+++ online/class-four/ex58.js
@@ -0,0 +1,6 @@
+// Queremos que o arquivo series.js processe TODOS os pedidos do recurso. Como primeiro passo pedimos que você crie a variável express e
+// armazene o módulo express nela. Então, você deve criar a variável router e armazenar nela a execução do método que nos permite 
+// gerenciar um sistema de rotas.
+
+const express = require("express");
+const router = express.Router();
