4810e482091fc1e86040b01860d21b6f72f0df6b
mh-fs <matheus.henrique1021@gmail.com>
2020-04-05 22:25:49 -0300

    Hundred-twentieth online exercise

---
+++ online/class-seven/ex120.js
@@ -0,0 +1,9 @@
+// Agora vamos trabalhar com POST. Você se lembra que as URL do tipo POST são utilizadas para guardar informações em nossa base de dados ou mandar informações sensíveis. Para recuperar os dados enviados por POST nós utilizamos o atributo body do objeto request. Mas antes é necessário indicar ao express que vamos trabalhar com JSON e que a informação seja enviada no formato correto. Portanto é necessário executar duas linhas de código de acordo com o método use do objeto app.
+
+// Já tendo criado a constante app use-a para adicionar a configuração necessária (urlencoded e json) para trabalhar com post.
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
