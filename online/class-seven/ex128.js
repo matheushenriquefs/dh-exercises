45eefc541a6a4d62f44d4c24c9ef336374e7fabd
mh-fs <matheus.henrique1021@gmail.com>
2020-04-26 21:37:03 -0300

    Hundred-twenty-eight online exercise

---
+++ online/class-seven/ex128.js
@@ -0,0 +1,8 @@
+// Vamos explorar um pouco o módulo PATH. Este módulo nos fornece uma maneira fácil de trabalhar com diretórios e caminhos de arquivos.
+
+// Para começar, crie uma constante path usando const e atribua a ela o módulo path do Node.
+
+// Depois, faço um console.log para mostrar o conteúdo da constante path definida anteriormente.
+
+const path = require("path");
+console.log(path);
