c4865448816a691299cc19c8949fc898b33c33fb
mh-fs <matheus.henrique1021@gmail.com>
2020-04-26 21:52:26 -0300

    132nd online exercise

---
+++ online/class-seven/ex132.js
@@ -0,0 +1,8 @@
+// O módulo file system nos permitirá ler e gravar arquivos no sistema operacional. O uso deste pacote é tão importante que ele já está instalado com o NodeJS.
+
+// Para começar, crie a constante fs usando const e atribua a ele o require do "fs".
+
+// Para finalizar, faça um console.log da constante fs.
+
+const fs = require("fs");
+console.log(fs);
