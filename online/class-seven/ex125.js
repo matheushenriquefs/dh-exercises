0c5600f55482ea9181dc2a61d3bd203c42858085
mh-fs <matheus.henrique1021@gmail.com>
2020-04-26 19:20:40 -0600

    Hundred-twenty-fifth online exercise

---
+++ online/class-seven/ex125.js
@@ -0,0 +1,47 @@
+// Temos um Json com três filmes
+
+// Vamos tratar de criar uma ruta para editar os dados de um filme em particular. Um usuário vai ter de ser capaz de enviar dados através de um formulário e nós devemos editar a película escolhida. Para indicar a película a ser modificada vamos receber a url e a id correspondente.  Em outras palavras, devemos criar um caminho do tipo PUT para os casos filme/1, filme/2, etc.
+
+// Para isso vamos ter que criar a partir do objeto router uma rota do tipo PUT que responsa a qualquer uma das urls mencionadas anteriormente.
+// Devemos ser capazes de identificar o filme, dependendo do ID que nos é passado como parâmetro, e atualizar seus dados com os que nos são enviados.
+
+let filmes = [
+    {
+        id: 1,
+        titulo: 'Spider-Man: Longe de casa',
+        duracao: 129,
+        genero: 'Aventura'
+    },
+    {
+        id: 2,
+        titulo: 'Toy Story 4',
+        duracao: 100,
+        genero: 'Animacao'
+    },
+    {
+        id: 3,
+        titulo: 'X-Men: Fênix Negra',
+        duracao: 113,
+        genero: 'Acao'
+    }
+];
+
+const express = require('express');
+const router = express.Router();
+
+router.put("/filme/:id", (req, res) => {
+
+    for(x of filmes){
+
+        if(x.id === req.params.id){
+            
+            x.titulo = req.body.titulo;
+            x.duracao = req.body.duracao;
+            x.genero = req.body.genero;
+
+        }
+
+        res.send(filmes);
+
+    }
+});
